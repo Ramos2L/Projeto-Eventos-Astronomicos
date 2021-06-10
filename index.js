cdf0f8e9221380635b411097ea191dde8b2845e9
Ramos <58201578+Ramos2L@users.noreply.github.com>
2021-06-09 22:28:09 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import chuvasDeMeteoros from "./data/chuvas-de-meteoros.js";
+import imprimeListaChuva from "./funcoes/funcoesInterface.js";
+import { verificaChuvaOcorrendo, verificaProximaChuva } from "./funcoes/funcoesLogicas.js";
+
+const dataAtual = new Date();
+
+const chuvasVisiveis = chuvasDeMeteoros.filter( (chuva)=> verificaChuvaOcorrendo(chuva, dataAtual) );
+const proximaChuva = chuvasDeMeteoros.filter( (chuva)=> verificaProximaChuva(chuva, dataAtual) );
+
+console.log('Chuva de meteoros');
+
+if ( chuvasVisiveis.length > 0 ) {
+    let msg = "\nEncontramos ";
+    msg += chuvasVisiveis.length == 1 ? '1 chuva de meteoros' :
+    chuvasVisiveis.length + ' chuvas de meteoros';
+
+    console.log(msg);
+    imprimeListaChuva(chuvasVisiveis)
+} else {
+    console.log('\nNenhuma chuva de meteoros passando no momento');
+}
+
+console.log('\n\nNão perca as próximas chuvas de meteoros:');
+imprimeListaChuva(proximaChuva);
+
